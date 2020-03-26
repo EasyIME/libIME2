704e02c96a05d0faa6d04ec739e16c8f57202ee0
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2020-03-27 01:41:27 +0800

    Test ComPtr using ComObject.

--- test/ComPtr_test.cpp
+++ test/ComPtr_test.cpp
@@ -3,25 +3,13 @@
 
 #include <unknwn.h>
 #include "ComPtr.h"
+#include "ComObject.h"
 
 using ::testing::Return;
 
-class IUnknownMock : public IUnknown {
+class IUnknownMock : public Ime::ComObject<> {
 public:
-    IUnknownMock() : refCount_{ 0 } {}
-
-    ULONG STDMETHODCALLTYPE AddRef() override {
-        ++refCount_;
-        return 0;
-    }
-    ULONG STDMETHODCALLTYPE Release() override {
-        --refCount_;
-        return 0;
-    }
-
     MOCK_METHOD(HRESULT, QueryInterface, (REFIID riid, void** ppvObject), (Calltype(STDMETHODCALLTYPE), override));
-
-    int refCount_;
 };
 
 TEST(TestComPtr, DefaultsToNull)
@@ -35,16 +23,15 @@ TEST(TestComPtr, InitFromIUnknownPtr)
 	IUnknownMock obj;
     Ime::ComPtr<IUnknownMock> ptr(&obj);
     EXPECT_EQ(ptr, &obj);
-    EXPECT_EQ(obj.refCount_, 1);
+    EXPECT_EQ(obj.refCount(), 2);
 }
 
 TEST(TestComPtr, TakesOverIUnknownPtr)
 {
-    IUnknownMock obj;
-
-    Ime::ComPtr<IUnknownMock> ptr(&obj, false);
-    EXPECT_EQ(ptr, &obj);
-    EXPECT_EQ(obj.refCount_, 0);
+    auto obj = new IUnknownMock();
+    Ime::ComPtr<IUnknownMock> ptr(obj, false);
+    EXPECT_EQ(ptr, obj);
+    EXPECT_EQ(obj->refCount(), 1);
 }
 
 TEST(TestComPtr, Destructor)
@@ -52,19 +39,19 @@ TEST(TestComPtr, Destructor)
     IUnknownMock obj;
     {
         Ime::ComPtr<IUnknownMock> ptr(&obj);
-        EXPECT_EQ(obj.refCount_, 1);
+        EXPECT_EQ(obj.refCount(), 2);
     }
-    EXPECT_EQ(obj.refCount_, 0);
+    EXPECT_EQ(obj.refCount(), 1);
 }
 
 TEST(TestComPtr, AddRefOnCopy)
 {
     IUnknownMock obj;
     Ime::ComPtr<IUnknownMock> ptr{&obj};
-    EXPECT_EQ(obj.refCount_, 1);
+    EXPECT_EQ(obj.refCount(), 2);
 
     Ime::ComPtr<IUnknownMock> ptr2{ ptr };
-    EXPECT_EQ(obj.refCount_, 2);
+    EXPECT_EQ(obj.refCount(), 3);
     EXPECT_EQ(ptr2, &obj);
 }
 
@@ -72,10 +59,10 @@ TEST(TestComPtr, MoveConstructor)
 {
     IUnknownMock obj;
     Ime::ComPtr<IUnknownMock> ptr{ &obj };
-    EXPECT_EQ(obj.refCount_, 1);
+    EXPECT_EQ(obj.refCount(), 2);
 
     Ime::ComPtr<IUnknownMock> ptr2{ std::move(ptr) };
-    EXPECT_EQ(obj.refCount_, 1);
+    EXPECT_EQ(obj.refCount(), 2);
     EXPECT_EQ(ptr, nullptr);
     EXPECT_EQ(ptr2, &obj);
 }
