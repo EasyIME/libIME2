2b6667bbd942f2571f6e54e33f2f81558f7d4658
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2020-03-29 02:22:06 +0800

    Prevent redefine WIN32_LEAN_AND_MEAN macro.

--- src/libIME.h
+++ src/libIME.h
@@ -32,7 +32,10 @@
 #define LIBIME_API __declspec(dllimport)
 #endif
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif // WIN32_LEAN_AND_MEAN
+
 // Windows Header Files:
 #include <windows.h>
 
